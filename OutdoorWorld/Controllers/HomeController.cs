07c95813eaca7dfb832b460c9822f81e37477438
Carl Atherton <ATHERTOC@my.erau.edu>
2020-09-11 23:44:59 -0700

    Updated  SportsStore

---
+++ OutdoorWorld/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OutdoorWorld.Models;
+using System.Security.Permissions;
+
+namespace OutdoorWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index() => View(repository.Products);
+    }
+}
