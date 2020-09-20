ba67a471f8e27d05c8dc8a3e03a80f892af0b851
Carl Atherton <68295667+AthertonCarl@users.noreply.github.com>
2020-09-19 20:11:27 -0700

    Updated ScreenShots

---
+++ OutdoorWorld/Componets/NavigationMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using OutdoorWorld.Models;
+
+namespace OutdoorWorld.Componets
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private  IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCAtegory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
